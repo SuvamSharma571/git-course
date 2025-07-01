ecdab2e37d92843e5792d46b74ca474a8d72328e
Suvam Sharma <158548329+SuvamSharma571@users.noreply.github.com>
2025-07-01 05:59:51 +0000

    For variables and datatypes

---
+++ Javascript_basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all js code as newer strict
+//alert("hello")// we are using node ja and not browser
+let name = "suabam"
+let age = 18
+let isLoggedin = "false"
+
+
+// Number =>2 to power 53
+//bigint very large integer
+//string = single or double quote
+// boolean = true or false
+//null = standalone value when checked for type of . it shows object
+//undefined = same type when checked with typeof
+//symbol = uniquess 
+
+//object 
+console.log(typeof(name))
---
+++ Javascript_basics/variables.js
@@ -0,0 +1,12 @@
+const acc_id=144553
+let acc_email = "hitesh@google.com"
+var acc_pass ="12345"
+account_city = "Bhubaneswar"
+ 
+ //acc_id = 2
+ console.log(acc_id)
+ acc_email = "Suvam2@su.ac.in"
+ acc_pass = "211211"
+ account_city = "Bahalda"
+ const acc_add = 22;
+ console.table([acc_email, acc_pass, account_city,acc_add])
