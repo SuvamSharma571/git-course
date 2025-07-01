ef2d457425d386362e4132cb04bd73d163581039
Suvam Sharma <158548329+SuvamSharma571@users.noreply.github.com>
2025-07-01 07:46:00 +0000

    comparison and datatypes revision

---
+++ Javascript_basics/comparison.js
@@ -0,0 +1,15 @@
+console.log("2"<true)
+console.log("1">false)
+
+console.log(null>1)
+console.log(null<1)
+console.log(null == 0)
+console.log(null>=0)
+
+
+console.log(undefined >1)
+
+// In javascript == and >=,< comparison checks are different 
+// For comparison check the data gets converted to number such as null but not in equality check 
+
+// === strict check ... it also checck for datatypes 
---
+++ Javascript_basics/datatypes_summ.js
@@ -0,0 +1,26 @@
+//primitive and non primitive -- this conversion is done keeping in mind how data is saved and how it is being accessed
+//primitive(call by value) - 7 types - string, Number , Boolean , Null , Undefiiined , symbol , bigint 
+
+// Non primitive or reference type ( call by reference)  - arrays, objects, functions  ---- return object or function object
+
+// Java Script is a dynamically typed language
+const age = 2
+const score = 2.3
+const name = "Suvam"
+let name1;
+const id = Symbol('123')
+const aid = Symbol('123')
+console.log(id === aid)
+const bignumber = 234565432345434565434543454343234323432323432343434n
+
+
+const heros = ["Shaktiman", "naagraj", "doaga"]
+let myobj = { 
+    age: 23,
+    name1 :"Suvam",
+}
+
+const myfunct = function (){
+    console.log ("Hello world");
+}
+console.log(myfunct())
