f74dc2868e9067892a7555034215fe58aa60ca12
Suvam Sharma <158548329+SuvamSharma571@users.noreply.github.com>
2025-07-01 07:05:50 +0000

    deep invwstigation on conversion

--- Javascript_basics/conversion.js
+++ Javascript_basics/conversion.js
@@ -9,3 +9,23 @@ console.log(typeof(na))
 console.log (na)
 // When trying to convert a null value to integer it gives a value of 0
 
+// *******************************operation********************************
+
+let value = 3
+let negvalue= -value
+console.log (value)
+console.log(negvalue)
+
+let str= "hello"
+let str1= " hello "
+let str2 = str+str1
+console.log (str2)
+console.log("1" + 2)
+console.log (3+4*5%6)
+
+console.log(+true)
+console.log(+"")
+let num1,num2,num3
+
+num1= num2= num3 = 2 + 32
+
