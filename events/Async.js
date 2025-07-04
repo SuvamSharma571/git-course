7d966933dac61915353dd43fd8093c2209e08a08
Suvam Sharma <158548329+SuvamSharma571@users.noreply.github.com>
2025-07-04 15:47:37 +0000

    Events

---
+++ events/Async.js
@@ -0,0 +1,5 @@
+// synchronous , single threaded, ---default
+// Execution context one line after one excute 
+// blocking code vs non blocking code
+// block the flow of the code////////// Read file Async
+// Read the file sync
