643d7078c26dab5360793721728c37ea156d54af
Suvam Sharma <158548329+SuvamSharma571@users.noreply.github.com>
2025-07-04 15:21:50 +0000

    eventlistener

---
+++ events/eventbasics.js
@@ -0,0 +1,27 @@
+// Java script is a sequentially runned language.
+//Event is any thing done activity in the browser
+ //document.getElementbyif().onclick = not so good
+ //attachEvent()
+ //jquery -oneventlistner
+
+  // document.getElementbyId().addEventListener('click',fucntion(e){
+ // your work -- },false)
+ //types,timestsmp,preventdefault, target,toelement, srcelement
+ //currenttarget
+ //clientposition, sceenvalue
+ //alt key , ctrl , shift key , keycode
+
+ //Eventpropagation-event bubbling false low to high ...inside to outside  , event capturing true to to bottom high to low 
+ //stop porpagation... will not bubble up and it willl be on parameter
+
+ //ducumnet.queryselector('#images).addEventListener('click',
+ //(e)=>
+    //e.target.parentNode
+ //if(e.target.tagname == 'img')
+
+ //letmoveit = e.target.parentNode
+ //moveit.remove()
+ //moveit.parentNode.removechild(removeit)
+ //)
+
+
